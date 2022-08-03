5b57c81231bb7fb417d8a7e9b57501e55b977206
team01member-clare <110090204+team01member-clare@users.noreply.github.com>
2022-08-03 22:15:02 +0800

    Add files via upload

--- api.py
+++ api.py
@@ -1,20 +1,18 @@
-import requests 
-from pathlib import Path 
-import csv 
-def api(): 
-        url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey=XJ54Q43467ETNK7Y" 
-        response = requests.get(url) 
-        data = response.json() 
-        conversion = data["Realtime Currency Exchange Rate"] 
-        conversion_list = [] 
-        for rate in conversion: 
-                exchangerate = conversion ["5. Exchange Rate"] 
-                exchangerate= f"[REAL TIME CURRENCY EXCHANGE RATE] USD1 = SGD{exchangerate}" 
-                print(exchangerate) 
-                cwd= Path.cwd() 
-                summary = cwd/"summary_report.txt" 
-                summary.touch() 
-                with summary.open(mode="w", encoding="UTF-8",newline="")as file: 
-                        writer=csv.writer(file) 
-                        writer.writerow([exchangerate]) 
-print(api()) 
+import requests
+from pathlib import Path
+import csv
+def api():
+        url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey=XJ54Q43467ETNK7Y"
+        response = requests.get(url)
+        data = response.json()
+        conversion = data["Realtime Currency Exchange Rate"]
+        exchangerate = conversion ["5. Exchange Rate"]
+        exchangerate= f"[REAL TIME CURRENCY EXCHANGE RATE] USD1 = SGD{exchangerate}"
+        print(exchangerate)
+        cwd= Path.cwd()
+        summary = cwd/"summary_report.txt"
+        summary.touch()
+        with summary.open(mode="w", encoding="UTF-8",newline="")as file:
+                writer=csv.writer(file)
+                writer.writerow([exchangerate])
+print(api())
