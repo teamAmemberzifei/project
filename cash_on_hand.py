2b237188f207d06fa1fa7e7982f5a5c10a291205
teams01memberjiaen <110022229+teams01memberjiaen@users.noreply.github.com>
2022-08-03 23:58:52 +0800

    Update cash_on_hand.py

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,35 +1,75 @@
 from pathlib import Path
 import csv
 import requests
+
+#create a function
 def cash():
+
+    #use the function and api given to find the conversion from USD to SGD
     url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey=XJ54Q43467ETNK7Y"
+    
+    #use .get() to retrive data from server
     response = requests.get(url)
+
+    #use .json() to retrive data and store as JSON object
     data = response.json()
+
+    #extract the keys of the dict to retrive the exchange rate
     conversion = data["Realtime Currency Exchange Rate"]
     #print(conversion)
+
+    #extract the data from the keys to retrive the exchange rate 
     exchangerate = conversion ["5. Exchange Rate"]
+
+    #exchange rate have to be a float in order to multipy 
     exchangerate=float(exchangerate)
     #print(exchangerate)
+
+    #create a cwd to check on my current location in the file system 
     cwd= Path.cwd()
+
+    #create a file path to the cash on hand csv to extract data
     cash_on_hand = cwd/"csv_reports"/"Cash_on_Hand.csv"
+
+    #create an empty list for the days and cash
     days=[]
     cash=[]
-    cash_on_hand = cwd/"csv_reports"/"Cash_on_Hand.csv"
+
+    #using with statement and reading mode to read the data from the csv 
     with cash_on_hand.open(mode="r",encoding="UTF-8",newline="") as file:
+
+        #instantiate a reader object
         reader = csv.reader(file)
+
+        #next is used to skip the header
         next(reader)
+
+        #create a for loop to extract all the data into one list
         for value in reader:
+
+            #extract the days from the data
             values = value[0]
+
+            #create the day into a float to append the days
             float1 = float(values)
             days.append(float1)
+
+            #extract the value from the data
             values1 = value[1]
+
+            #create the value into a float to append the values
             float2 = float(values1)
             cash.append(float2)
-    #print(cash)
-    #print(days)
+    
+    #use the for loop to extract all the data that met the if condition 
     for value in cash:
+
+        #if the cash on hand of the prev day is more than current day , the program will run and print the following
+        #it is the same throughout 
         if cash[0]>cash[1]:
             diff = cash[0]-cash[1]
+
+            #multiply the exchange rate with the difference of the value between 2 days to convert USD to SGD
             ans1 = f"[CASH SURPLUS] DAY:{days[1]}, AMOUNT: SGD{exchangerate*diff}"
             print(ans1)
         if cash[1]>cash[2]:
@@ -68,12 +108,25 @@ def cash():
            diff9 = cash[9]-cash[10]
            ans10 = f"[CASH SURPLUS] DAY:{days[10]}, AMOUNT: SGD{exchangerate*diff9}"
            print(ans10)
-        break
 
-    coh = cwd/"summary_report.txt"
-    coh.touch()
-    with coh.open(mode="w", encoding="UTF-8",newline="")as file:
-        writer=csv.DictWriter(file, fieldnames=[ans1,ans5,ans9,ans10])
-        writer.writeheader()
-    file.close()
+        #use break to stop an iteration 
+        break
+    
+    #create a new text file to input the summarized data  
+    summary = cwd/"summary_report.txt"
+
+    #create a new file with touch
+    summary.touch()
+
+    #use with text with append to add on the data to the summary page
+    with summary.open(mode="a", encoding="UTF-8",newline="")as file:
+
+        #create a writer object and named it as writer
+        writer=csv.writer(file)
+
+        #use writerow to print the answer
+        writer.writerow([ans1])
+        writer.writerow([ans5])
+        writer.writerow([ans9])
+        writer.writerow([ans10])
 print(cash())
